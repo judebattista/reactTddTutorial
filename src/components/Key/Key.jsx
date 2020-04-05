7b11fb72dcfc2e09ab519736c847082c7123e39d
Jude Battista <jbattista20@my.whitworth.edu>
2020-04-04 17:36:39 -0700

    Number keys connected to display output

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import './Key.css';
 
 const Key = ({ keyAction, keyType, keyValue }) => (
-    <div className={`key-container ${keyType}`}> 
-        <p classname="key-value">
+    <div 
+        className={`key-container ${keyType}`} 
+        onClick={() => keyAction(keyValue)} 
+    >
+        <p className="key-value">
             {keyValue}
         </p>    
     </div>
